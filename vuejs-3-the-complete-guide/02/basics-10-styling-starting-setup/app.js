d925ed229b405ff18709bf2191bd10a1a38f061e
BAHK Hyeongjun <hbahk42@outlook.com>
2022-09-22 11:04:53 +0900

    complete 'basics-10-styling-starting-setup'

--- vuejs-3-the-complete-guide/02/basics-10-styling-starting-setup/app.js
+++ vuejs-3-the-complete-guide/02/basics-10-styling-starting-setup/app.js
@@ -1,40 +1,7 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            boxSelected: [false, false, false]
-        }
-    },
-    watch: {
-        boxSelected: {
-            handler(value) {
-                console.log(value);
-                // styleA = value ? 'red' : '#ccc';
-            },
-            deep: true
-        }
-    },
     methods: {
-        clickBox(event) {
-            switch (event.target.id) {
-                case 'box-a':
-                    this.setOpposite(0);
-                    break;
-                case 'box-b':
-                    this.setOpposite(1);
-                    break;
-                case 'box-c':
-                    this.setOpposite(2);
-                    break;
-                default:
-                    break;
-            }
-        },
-        setOpposite(index) {
-            this.boxSelected[index] = !this.boxSelected[index];
-        },
-        getStyle(index) {
-            console.log(this.boxSelected[index]);
-            return this.boxSelected[index] ? 'red' : '#ccc';
+        selectBox(event) {
+            event.target.classList.toggle('selected')
         }
     }
 });
