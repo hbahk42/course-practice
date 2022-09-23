ba5385d759494d97a89188d85a3b3f1b03c43261
BAHK Hyeongjun <hbahk42@outlook.com>
2022-09-23 17:14:17 +0900

    complete 'behind-scenes'

--- vuejs-3-the-complete-guide/05/behind-scenes-01-starting-setup/app.js
+++ vuejs-3-the-complete-guide/05/behind-scenes-01-starting-setup/app.js
@@ -1,18 +1,88 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
+      // currentUserInput: '',
       message: 'Vue is great!',
     };
   },
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
+    // saveInput(event) {
+    //   this.currentUserInput = event.target.value;
+    // },
+    // setText() {
+    //   this.message = this.$refs.userInput;
+    // },
     setText() {
-      this.message = this.currentUserInput;
+      this.message = this.$refs.userInput.value;
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  }
 });
 
 app.mount('#app');
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
+
+const app2 = Vue.createApp({
+  template: `
+    <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: 'Pizza'
+    };
+  }
+});
+
+app2.mount('#app2');
+
+// The below is not controlled by Vue.
+const data = {
+  message: 'Hello',
+  longMessage: 'Hello, World!'
+};
+
+const handler = {
+  // set(target, key, value) {
+  //   console.log(target);
+  //   console.log(key);
+  //   console.log(value);
+  // }
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ', World!';
+    }
+    target.message = value;
+  }
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Goodbye'; // Goodbye, World!
+
+console.log(proxy.longMessage);
