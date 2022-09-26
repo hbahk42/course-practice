92f76de5feeb47054223bc5927ae6a57c9fe94a5
BAHK Hyeongjun <hbahk42@outlook.com>
2022-09-26 17:16:52 +0900

    finish 'cmp-communication-assignment-problem'

---
+++ 08/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <h2>{{ user.name }}</h2>
+        <h3>{{ user.age }} Years</h3>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            name: {
+                type: String,
+                required: true
+            },
+            age: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+};
+</script>
